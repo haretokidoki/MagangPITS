2627509481fae700670172c21f892cb9e6db7d38
haretokidoki <natanaeljunior582@gmail.com>
2023-02-09 11:19:09 +0700

    documents

--- src/main.js
+++ src/main.js
@@ -2,9 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import UniqueId from 'vue-unique-id'
+//import socketio from 'socket.io';
+//import VueSocketIO from 'vue-socket.io';
+import router from './router/router'
+
+//export const SocketInstance = socketio('http://35.202.174.163:3030/');
+//const UniqueId = require('vue-unique-id')
 
 loadFonts()
 
 createApp(App)
-  .use(vuetify)
+//     {router,
+//     render: h => h(App)
+//     }
+
+  .use(vuetify, UniqueId //VueSocketIO, SocketInstance
+  )
   .mount('#app')
+
+//createApp.config.productionTip = false
